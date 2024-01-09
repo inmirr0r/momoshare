69fa6d93ab4180b06616dd4ef956e0b2aea82ff6
inmirr0r <155992664+inmirr0r@users.noreply.github.com>
2024-01-09 08:19:44 +0800

    Update MMSLS.js

--- MMSLS.js
+++ MMSLS.js
@@ -20,7 +20,7 @@ const MMSLS = [
  */
 const MMSLS = [
     
-    "https://www.maimemo.com/share/page?uid=32454734&pid=4ddc9601f97ade55d199f454c522b81b&tid=362a2ecbf9553a116d5eba36b50f582a",
+    "https://www.maimemo.com/share/page?uid=33498357&pid=820132de40e3ea244ff8c219d3917ef8&tid=9037a1bdecc5ccc332f2e5d1c0d6689b",
 
 
   ]
